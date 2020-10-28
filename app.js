a2331958966883fa5148ebdf34a1adb60c33eca3
parikshitdas10 <parikshitdas10@gmail.com>
2020-10-28 15:01:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var MenuItems = document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+  if (MenuItems.style.maxHeight == "0px") {
+    MenuItems.style.maxHeight = "200px";
+  } else {
+    MenuItems.style.maxHeight = "0px";
+  }
+}
+const options = {
+  bottom: "32px", // default: '32px'
+  right: "32px", // default: '32px'
+  left: "unset", // default: 'unset'
+  time: "0.5s", // default: '0.3s'
+  mixColor: "#fff", // default: '#fff'
+  backgroundColor: "#fff", // default: '#fff'
+  buttonColorDark: " #ff523b", // default: '#100f2c'
+  buttonColorLight: "#00e6e6", // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: "🌓", // default: ''
+  autoMatchOsTheme: true, // default: true
+};
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
